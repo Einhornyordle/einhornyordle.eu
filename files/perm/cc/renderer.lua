00d2beb47f343156b995579228d81c446eed7c5c
Einhornyordle <einhornyordle@einhornyordle.eu>
2023-01-25 21:48:37 +0100

    Small fixes for renderer script

--- files/perm/cc/renderer.lua
+++ files/perm/cc/renderer.lua
@@ -1,10 +1,10 @@
-peripherial.find("modem").open(25890)
+peripheral.find("modem").open(25890)
 
 while true do
     msg = {os.pullEvent("modem_message")}
     term.clear()
     term.setCursorPos(1, 1)
-    if msg[5][sender] == "ECoreManager" then
+    if msg[5]["sender"] == "ECoreManager" then
         print("Stored Energy: " .. msg[5]["energyStored"] .. "rf")
         print("Energy Capacity: " .. msg[5]["maxEnergyStored"] .. "rf")
         print("Current Transfer: " .. msg[5]["transferPerTick"] .. "rf/t")
