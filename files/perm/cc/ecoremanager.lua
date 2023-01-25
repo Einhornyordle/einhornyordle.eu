0c02042912e5e590801b080435329c094b13904a
Einhornyordle <einhornyordle@einhornyordle.eu>
2023-01-25 20:57:38 +0100

    Add ecoremanager cc script

---
+++ files/perm/cc/ecoremanager.lua
@@ -0,0 +1,18 @@
+term.clear()
+local modem = peripheral.find("modem")
+local core = peripheral.find("draconic_rf_storage")
+
+if core == nil and modem == nil then
+    print("You need to connect a wireless modem and the desconic core first")
+    return
+end
+
+local data = {}
+data["sender"] = "ECoreManager"
+while true do
+    data["energyStored"] = core.getEnergyStored()
+    data["maxEnergyStored"] = core.getMaxEnergyStored()
+    data["transferPerTick"] = core.getTransferPerTick()
+    modem.transmit(25890, 25890, data)
+    os.sleep(0.5)
+end
