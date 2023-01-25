d93015ef67f2df46e1672b0bf8ca3a12abe1c408
Einhornyordle <einhornyordle@einhornyordle.eu>
2023-01-25 22:56:48 +0100

    Small rewrite for consistency reasons

--- files/perm/cc/ecoremanager.lua
+++ files/perm/cc/ecoremanager.lua
@@ -1,19 +1,30 @@
-term.clear()
-term.setCursorPos(1, 1)
+local data = {
+    name = "Crystal_ECore_Manager",
+    type = "ecore",
+    energyStored = 0,
+    maxEnergyStored = 0,
+    transferPerTick = 0
+}
 local modem = peripheral.find("modem")
 local core = peripheral.find("draconic_rf_storage")
 
-if core == nil and modem == nil then
-    print("You need to connect a wireless modem and the desconic core first")
+if core == nil then
+    term.setTextColor(16384)
+    print("Error 404: Core not found")
+    term.setTextColor(1)
     return
 end
 
-local data = {}
-data["sender"] = "ECoreManager"
 while true do
     data["energyStored"] = core.getEnergyStored()
     data["maxEnergyStored"] = core.getMaxEnergyStored()
     data["transferPerTick"] = core.getTransferPerTick()
     modem.transmit(25890, 25890, data)
+    term.clear()
+    term.setCursorPos(1, 1)
+    print(data.name)
+    print("Stored Energy: " .. data["energyStored"] .. "rf")
+    print("Energy Capacity: " .. data["maxEnergyStored"] .. "rf")
+    print("Current Transfer: " .. data["transferPerTick"] .. "rf/t")
     os.sleep(0.5)
 end
