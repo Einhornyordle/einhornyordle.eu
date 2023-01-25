333223ebf6d482a0a08dded3e7c359881574a7c1
Einhornyordle <einhornyordle@einhornyordle.eu>
2023-01-25 21:28:58 +0100

    Add renderer cc script

---
+++ files/perm/cc/renderer.lua
@@ -0,0 +1,12 @@
+peripherial.find("modem").open(25890)
+
+while true do
+    msg = {os.pullEvent("modem_message")}
+    term.clear()
+    term.setCursorPos(1, 1)
+    if msg[5][sender] == "ECoreManager" then
+        print("Stored Energy: " .. msg[5]["energyStored"] .. "rf")
+        print("Energy Capacity: " .. msg[5]["maxEnergyStored"] .. "rf")
+        print("Current Transfer: " .. msg[5]["transferPerTick"] .. "rf/t")
+    end
+end
